99ab1a681fe93fc2a69337a681e970b35b453534
Minf97 <287728237@qq.com>
2023-11-02 18:16:36 +0800

    全局模块+生命周期

--- src/aaa/aaa.module.ts
+++ src/aaa/aaa.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { AaaService } from './aaa.service';
 import { AaaController } from './aaa.controller';
 
+@Global()
 @Module({
   controllers: [AaaController],
   providers: [AaaService],
--- src/bbb/bbb.module.ts
+++ src/bbb/bbb.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BbbService } from './bbb.service';
 import { BbbController } from './bbb.controller';
-import { AaaModule } from 'src/aaa/aaa.module';
 
 @Module({
-  imports: [AaaModule],
+  imports: [],
   controllers: [BbbController],
   providers: [BbbService],
 })
