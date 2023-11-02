f40520bb073c7db41ae3aa9dc9899919d105c469
Minf97 <287728237@qq.com>
2023-11-02 11:29:02 +0800

    url param + query

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,34 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
 
-@Controller('person')
+@Controller('api/person')
 export class PersonController {
-  constructor(private readonly personService: PersonService) {}
+  constructor(private readonly personService: PersonService) { }
 
-  @Post()
-  create(@Body() createPersonDto: CreatePersonDto) {
-    return this.personService.create(createPersonDto);
+  // 2. query
+  // api/person/find?xxx
+  // 注意书写顺序，要放在1的前面
+  @Get('find')
+  query(@Query('name') name: string, @Query('age') age: number) {
+    return `received: name=${name}, age=${age}`
   }
 
-  @Get()
-  findAll() {
-    return this.personService.findAll();
-  }
-
+  // 1. url param
+  // api/person/xxx
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.personService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
-    return this.personService.update(+id, updatePersonDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.personService.remove(+id);
-  }
+  urlParam(@Param('id') id: string) {
+    return `received: id:${id}`
+  }  
 }
